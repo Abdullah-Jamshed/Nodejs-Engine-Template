d2279636c5b8dde6b3f8eb7da99aea804ddcdd0f
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-03-08 00:18:49 +0500

    nodejs-engine-template

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const hbs = require('hbs')
+
+const express = require("express");
+const app = express();
+
+// static
+const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+// app.use(express.static(staticPath));
+
+// set template engine
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+
+hbs.registerPartials(partialsPath)
+
+app.get("/", (req, res) => {
+  //   res.send("Hello from express");
+  res.render("index", { name: "Abdullah" });
+});
+
+
+app.get("/about", (req, res) => {
+    res.render("about", { name: "About" });
+  });
+  
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening at port ${PORT}`));
